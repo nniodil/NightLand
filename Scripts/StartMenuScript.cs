06bace187380b323b4643cea631cec404c2ba3a3
Nadir Niodil <165339957+nniodil@users.noreply.github.com>
2024-08-19 18:12:12 +0200

    Update StartMenuScript.cs

--- Scripts/StartMenuScript.cs
+++ Scripts/StartMenuScript.cs
@@ -4,17 +4,12 @@ using UnityEngine.SceneManagement;
 public class StartMenuScript : MonoBehaviour
 {
     // Start is called before the first frame update
-
     public void QuitButton()
     {
         Application.Quit();
-
     }
     public void StartButton()
     {
         SceneManager.LoadScene("Die Untoten");
-
     }
-
-
 }
